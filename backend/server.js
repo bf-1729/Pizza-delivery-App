a186ecf566fdf473885139334df6741e2768754b
lakshmi narasimha <mlakshminarasimh95a@gmail.com>
2025-03-11 10:58:17 +0530

    intial commit or updates

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ const app = express()
 app.use(express.json({ limit: '10mb' })); // Increase JSON size limit
 app.use(express.urlencoded({ limit: '10mb', extended: true })); // Increase URL-encoded data limit
 
-app.use(cors())
+app.use(cors({ origin: "*", credentials: true }));
+
 app.use("/api/pizzas/",pizzasRoute)
 app.use("/api/users/",userRoute)
 app.use("/api/orders/",orderRoute)
